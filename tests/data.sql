59daa8296f3ca113ce9c4dc5d0109d8512b76bc3
Krupin.SA <krupin.sergrey@gmail.com>
2020-07-02 18:24:25 +0500

    Add test sceleton

---
+++ tests/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO image (name, date)
+VALUES
+  ('cat1.jpg', '2018-01-01 00:00:00'),
+  ('cat2.jpg', '2018-01-01 00:00:00'),
+  ('cat3.jpg', '2018-01-01 00:00:00');
+
+INSERT INTO comment (text, image_id)
+VALUES
+  ('комментарий1', 1),
+  ('комментарий2', 1),
+  ('комментарий3', 2);
